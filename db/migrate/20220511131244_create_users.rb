09561f33e0874b38572f0d23d3287dcdf265daf8
Yan <yanfernandes@gmail.com>
2022-05-11 10:33:08 -0300

    create model user and migration

---
+++ db/migrate/20220511131244_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    enable_extension "pgcrypto"
+
+    create_table :users do |t|
+      t.binary :encrypted_email, null: false
+      t.text :email_digest, null:false
+      t.bigint :keyring_id, null:false
+
+      t.timestamps
+    end
+
+    add_index :users, :email_digest, unique: true 
+  end
+end
